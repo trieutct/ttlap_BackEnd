1a8456366064d86729aedc3946b54e747994d93b
trieutct <96654274+trieutct@users.noreply.github.com>
2024-01-26 14:21:35 +0700

    update @/ and src/

--- src/modules/prouduct/product.module.ts
+++ src/modules/prouduct/product.module.ts
@@ -4,33 +4,32 @@ import { ProductController } from './controllers/product.controller';
 import { ProductService } from './services/product.service';
 import { ProductRepository } from './product.repository';
 import { Product, ProductSchema } from '../../database/schemas/product.schema';
-import { MulterModule } from '@nestjs/platform-express';
-import { diskStorage } from 'multer';
-import * as path from 'path';
+// import { MulterModule } from '@nestjs/platform-express';
+// import { diskStorage } from 'multer';
 
 @Module({
     imports: [
         MongooseModule.forFeature([
             { name: Product.name, schema: ProductSchema },
         ]),
-        MulterModule.register({
-            storage: diskStorage({
-                destination: path.join(__dirname, '../../data'),
-                filename: (req, file, callback) => {
-                    callback(
-                        null,
-                        `${file.fieldname}-${Date.now()}-${file.originalname}`,
-                    );
-                },
-            }),
-            fileFilter: (req, file, callback) => {
-                if (file.mimetype.startsWith('image/')) {
-                    callback(null, true);
-                } else {
-                    callback(new Error('Not an image file'), false);
-                }
-            },
-        }),
+        // MulterModule.register({
+        //     storage: diskStorage({
+        //         destination: './data',
+        //         filename: (req, file, callback) => {
+        //             callback(
+        //                 null,
+        //                 `${file.fieldname}-${Date.now()}-${file.originalname}`,
+        //             );
+        //         },
+        //     }),
+        //     fileFilter: (req, file, callback) => {
+        //         if (file.mimetype.startsWith('image/')) {
+        //             callback(null, true);
+        //         } else {
+        //             callback(new Error('Not an image file'), false);
+        //         }
+        //     },
+        // }),
     ],
     controllers: [ProductController],
     providers: [ProductService, ProductRepository],
