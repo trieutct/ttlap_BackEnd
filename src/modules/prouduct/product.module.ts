7ca42082132f0ca5fdbda0d3e565685c8e910642
trieutct <96654274+trieutct@users.noreply.github.com>
2024-01-26 13:45:58 +0700

    fix vercel

--- src/modules/prouduct/product.module.ts
+++ src/modules/prouduct/product.module.ts
@@ -3,28 +3,33 @@ import { MongooseModule } from '@nestjs/mongoose';
 import { ProductController } from './controllers/product.controller';
 import { ProductService } from './services/product.service';
 import { ProductRepository } from './product.repository';
-import { Product,ProductSchema } from '../../database/schemas/product.schema';
+import { Product, ProductSchema } from '../../database/schemas/product.schema';
 import { MulterModule } from '@nestjs/platform-express';
 import { diskStorage } from 'multer';
 
 @Module({
     imports: [
-        MongooseModule.forFeature([{ name: Product.name, schema: ProductSchema }]),
-          MulterModule.register({
+        MongooseModule.forFeature([
+            { name: Product.name, schema: ProductSchema },
+        ]),
+        MulterModule.register({
             storage: diskStorage({
-              destination: './data',
-              filename: (req, file, callback) => {
-                callback(null, `${file.fieldname}-${Date.now()}-${file.originalname}`);
-              },
+                destination: '../data',
+                filename: (req, file, callback) => {
+                    callback(
+                        null,
+                        `${file.fieldname}-${Date.now()}-${file.originalname}`,
+                    );
+                },
             }),
             fileFilter: (req, file, callback) => {
-              if (file.mimetype.startsWith('image/')) {
-                callback(null, true);
-              } else {
-                callback(new Error('Not an image file'), false);
-              }
+                if (file.mimetype.startsWith('image/')) {
+                    callback(null, true);
+                } else {
+                    callback(new Error('Not an image file'), false);
+                }
             },
-          }),
+        }),
     ],
     controllers: [ProductController],
     providers: [ProductService, ProductRepository],
